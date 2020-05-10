79e73f7c1a940303b1060b043f88adc01c717f7d
bhokumar <bhoopendra.akgec@gmail.com>
2020-05-10 15:57:12 +0530

    Adding Projection

--- mongodb/normal_queries/inventory_data.js
+++ mongodb/normal_queries/inventory_data.js
@@ -1,7 +1,254 @@
-const data =[
-    { _id: 1, item: { name: "ab", code: "123" }, qty: 15, tags: [ "A", "B", "C" ] },
-    { _id: 2, item: { name: "cd", code: "123" }, qty: 20, tags: [ "B" ] },
-    { _id: 3, item: { name: "ij", code: "456" }, qty: 25, tags: [ "A", "B" ] },
-    { _id: 4, item: { name: "xy", code: "456" }, qty: 30, tags: [ "B", "A" ] },
-    { _id: 5, item: { name: "mn", code: "000" }, qty: 20, tags: [ [ "A", "B" ], "C" ] }
+const data = [{
+        _id: 1,
+        item: {
+            name: "ab",
+            code: "123"
+        },
+        qty: 15,
+        tags: ["A", "B", "C"]
+    },
+    {
+        _id: 2,
+        item: {
+            name: "cd",
+            code: "123"
+        },
+        qty: 20,
+        tags: ["B"]
+    },
+    {
+        _id: 3,
+        item: {
+            name: "ij",
+            code: "456"
+        },
+        qty: 25,
+        tags: ["A", "B"]
+    },
+    {
+        _id: 4,
+        item: {
+            name: "xy",
+            code: "456"
+        },
+        qty: 30,
+        tags: ["B", "A"]
+    },
+    {
+        _id: 5,
+        item: {
+            name: "mn",
+            code: "000"
+        },
+        qty: 20,
+        tags: [
+            ["A", "B"], "C"
+        ]
+    }
 ];
+
+
+db.inventory.insertMany([{
+        item: "journal",
+        qty: 25,
+        size: {
+            h: 14,
+            w: 21,
+            uom: "cm"
+        },
+        status: "A"
+    },
+    {
+        item: "notebook",
+        qty: 50,
+        size: {
+            h: 8.5,
+            w: 11,
+            uom: "in"
+        },
+        status: "A"
+    },
+    {
+        item: "paper",
+        qty: 100,
+        size: {
+            h: 8.5,
+            w: 11,
+            uom: "in"
+        },
+        status: "D"
+    },
+    {
+        item: "planner",
+        qty: 75,
+        size: {
+            h: 22.85,
+            w: 30,
+            uom: "cm"
+        },
+        status: "D"
+    },
+    {
+        item: "postcard",
+        qty: 45,
+        size: {
+            h: 10,
+            w: 15.25,
+            uom: "cm"
+        },
+        status: "A"
+    }
+]);
+
+
+db.inventory.insertMany([{
+        item: "journal",
+        qty: 25,
+        tags: ["blank", "red"],
+        dim_cm: [14, 21]
+    },
+    {
+        item: "notebook",
+        qty: 50,
+        tags: ["red", "blank"],
+        dim_cm: [14, 21]
+    },
+    {
+        item: "paper",
+        qty: 100,
+        tags: ["red", "blank", "plain"],
+        dim_cm: [14, 21]
+    },
+    {
+        item: "planner",
+        qty: 75,
+        tags: ["blank", "red"],
+        dim_cm: [22.85, 30]
+    },
+    {
+        item: "postcard",
+        qty: 45,
+        tags: ["blue"],
+        dim_cm: [10, 15.25]
+    }
+]);
+
+db.inventory.insertMany([{
+        item: "journal",
+        instock: [{
+            warehouse: "A",
+            qty: 5
+        }, {
+            warehouse: "C",
+            qty: 15
+        }]
+    },
+    {
+        item: "notebook",
+        instock: [{
+            warehouse: "C",
+            qty: 5
+        }]
+    },
+    {
+        item: "paper",
+        instock: [{
+            warehouse: "A",
+            qty: 60
+        }, {
+            warehouse: "B",
+            qty: 15
+        }]
+    },
+    {
+        item: "planner",
+        instock: [{
+            warehouse: "A",
+            qty: 40
+        }, {
+            warehouse: "B",
+            qty: 5
+        }]
+    },
+    {
+        item: "postcard",
+        instock: [{
+            warehouse: "B",
+            qty: 15
+        }, {
+            warehouse: "C",
+            qty: 35
+        }]
+    }
+]);
+
+
+db.inventory.insertMany([{
+        item: "journal",
+        status: "A",
+        size: {
+            h: 14,
+            w: 21,
+            uom: "cm"
+        },
+        instock: [{
+            warehouse: "A",
+            qty: 5
+        }]
+    },
+    {
+        item: "notebook",
+        status: "A",
+        size: {
+            h: 8.5,
+            w: 11,
+            uom: "in"
+        },
+        instock: [{
+            warehouse: "C",
+            qty: 5
+        }]
+    },
+    {
+        item: "paper",
+        status: "D",
+        size: {
+            h: 8.5,
+            w: 11,
+            uom: "in"
+        },
+        instock: [{
+            warehouse: "A",
+            qty: 60
+        }]
+    },
+    {
+        item: "planner",
+        status: "D",
+        size: {
+            h: 22.85,
+            w: 30,
+            uom: "cm"
+        },
+        instock: [{
+            warehouse: "A",
+            qty: 40
+        }]
+    },
+    {
+        item: "postcard",
+        status: "A",
+        size: {
+            h: 10,
+            w: 15.25,
+            uom: "cm"
+        },
+        instock: [{
+            warehouse: "B",
+            qty: 15
+        }, {
+            warehouse: "C",
+            qty: 35
+        }]
+    }
+]);
--- mongodb/normal_queries/mongo_inventory_queries.js
+++ mongodb/normal_queries/mongo_inventory_queries.js
@@ -1,3 +1,206 @@
-db.getCollection('inventory').find({"tags": {"$eq": ["A", "B"]}});
-db.getCollection('inventory').find({"item.name": {"$eq": "mn"}});
-db.getCollection('inventory').find({"item.name": {"$eq": "ab"}});
+db.getCollection('inventory').find({
+    "tags": {
+        "$eq": ["A", "B"]
+    }
+});
+db.getCollection('inventory').find({
+    "item.name": {
+        "$eq": "mn"
+    }
+});
+db.getCollection('inventory').find({
+    "item.name": {
+        "$eq": "ab"
+    }
+});
+
+db.getCollection('inventory').find({
+    size: {
+        h: 14,
+        w: 21,
+        uom: "cm"
+    }
+}); // Will Return a result
+db.getCollection('inventory').find({
+    size: {
+        w: 21,
+        h: 14,
+        uom: "cm"
+    }
+}); // Will not Return a result
+db.getCollection('inventory').find({
+    size: {
+        "$eq": {
+            h: 14,
+            w: 21,
+            uom: "cm"
+        }
+    }
+});
+
+db.getCollection('inventory').find({
+    'size.uom': 'in'
+}); // with dotted notation of field both must be wrapped under quotes
+
+db.getCollection('inventory').find({
+    "size.h": {
+        $lt: 15
+    }
+});
+
+db.getCollection('inventory').find({
+    "size.h": {
+        $lt: 15
+    },
+    "size.uom": "in",
+    status: "D"
+});
+
+db.getCollection('inventory').find({
+    "tags": {
+        "$eq": ["blank", "red"]
+    }
+});
+
+db.getCollection('inventory').find({
+    "tags": {
+        "$all": ["blank", "red"]
+    }
+});
+
+
+db.getCollection('inventory').find({
+    "tags": {
+        "$all": ["blank", "red"]
+    }
+});
+
+
+db.getCollection('inventory').find({
+    "dim_cm": {
+        "$gt": 15,
+        "$lt": 20
+    }
+}); // Matches any condition
+
+
+db.getCollection('inventory').find({
+    "dim_cm": {
+        "$elemMatch": {
+            "$gt": 15,
+            "$lt": 20
+        }
+    }
+}); // Matches Both Conditions
+
+
+db.getCollection('inventory').find({
+    "dim_cm.1": {
+        "$lt": 25
+    }
+});
+
+db.getCollection('inventory').find({
+    "dim_cm.0": {
+        "$eq": 10
+    }
+});
+
+db.getCollection('inventory').find({
+    "dim_cm": {
+        "$size": 3
+    }
+});
+
+
+db.getCollection('inventory').find({
+    "dim_cm": {
+        "$size": 2
+    },
+    "tags": {
+        "$size": 3
+    }
+});
+
+
+// Embedded documents in array
+
+db.getCollection('inventory').find({
+    "instock": {
+        "warehouse": "A",
+        "qty": 5
+    }
+});
+
+db.getCollection('inventory').find({
+    "instock.qty": {
+        "$eq": 5
+    }
+});
+
+db.getCollection('inventory').find({
+    "instock.1.qty": 5
+});
+
+db.getCollection('inventory').find({
+    "instock.1.qty": {
+        "$lt": 10
+    }
+});
+
+db.getCollection('inventory').find({
+    "instock": {
+        "warehouse": "B",
+        qty: {
+            "$gt": 5
+        }
+    }
+});
+
+
+db.getCollection('inventory').find({
+    "instock": {
+        qty: 5,
+        "warehouse": "A"
+    }
+});
+
+db.getCollection('inventory').find({
+    "instock": {
+        "$elemMatch": {
+            qty: 5,
+            "warehouse": "A"
+        }
+    }
+});
+
+db.getCollection('inventory').find({
+    "instock": {
+        "$elemMatch": {
+            qty: {
+                "$lt": 20,
+                "$gt": 10
+            }
+        }
+    }
+});
+
+db.inventory.find({
+    "instock.qty": {
+        $gt: 10,
+        $lte: 20
+    }
+});
+
+db.inventory.find({
+    status: "A"
+}, {
+    item: 1, status: 1, instock: 1
+});
+
+
+db.inventory.find({
+    status: "A"
+}, {
+    item: 1, status: 1, instock: 1, _id: 0
+});
