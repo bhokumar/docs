82f820b737200db9e889a77d9b256ec46a865a11
bhokumar <bhoopendra.akgec@gmail.com>
2020-05-13 20:44:37 +0530

    Adding files

---
+++ mongodb/normal_queries/students_data.js
@@ -0,0 +1,5 @@
+db.students.insertMany([
+    { _id: 1, test1: 95, test2: 92, test3: 90, modified: new Date("01/05/2020") },
+    { _id: 2, test1: 98, test2: 100, test3: 102, modified: new Date("01/05/2020") },
+    { _id: 3, test1: 95, test2: 110, modified: new Date("01/04/2020") }
+ ])
