65f8757e6c41b419db739d1ab8fccd23edc8a08d
bhokumar <bhoopendra.akgec@gmail.com>
2020-05-19 22:52:06 +0530

    Adding articles

---
+++ mongodb/normal_queries/articles_queries.js
@@ -0,0 +1,23 @@
+db.articles.aggregate([{
+        $match: {
+            $or: [{
+                score: {
+                    $gt: 70,
+                    $lt: 90
+                }
+            }, {
+                views: {
+                    $gte: 1000
+                }
+            }]
+        }
+    },
+    {
+        $group: {
+            _id: null,
+            count: {
+                $sum: 1
+            }
+        }
+    }
+]);
